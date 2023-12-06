63c62c87301914414579c19247fb5fe67dc6c1bf
Jay Miles <jay.miles@addenbrookes.nhs.uk>
2023-12-06 16:07:20 +0000

    coverage check added, additional ratio comparison added

--- src/code.sh
+++ src/code.sh
@@ -7,8 +7,8 @@ main() {
 
     dx-download-all-inputs
 
-    # get ids of all exon_stats files for that project
-    coverage_files=$(dx find data \
+    # get ids of all exon_stats files for the project
+    file_ids=$(dx find data \
     --name "*exon_stats.tsv" \
     --project "$hydra_input_project" \
     --brief)
@@ -18,37 +18,59 @@ main() {
     output_file="${output_dir}hydra_output_${hydra_input_project}.tsv"
 
     mkdir -p "$output_dir"
-    printf "sample\texon_3\texon_27\tratio\tover_threshold\n" > "$output_file"
+    printf "sample\texon_3\texon_5\texon_27\tratio_3_only\tratio_3_and_5\tpass_3_only\tpass_3_and_5\tpass_overall\t250x_issue\n" > "$output_file"
 
-    for coverage_file in $coverage_files; do
+    # evaluate each exon_stats file against exon coverage thresholds
+    for file_id in $file_ids; do
 
-        # get the file name, sample id, and archival state
-        state=$(dx describe "$coverage_file" --json | jq -r '.archivalState')
-        file_name=$(dx describe "$coverage_file" --json | jq -r '.name')
+        # get file name, sample id, and archival state
+        state=$(dx describe "$file_id" --json | jq -r '.archivalState')
+        file_name=$(dx describe "$file_id" --json | jq -r '.name')
         sample_id="${file_name%%_exon_stats.tsv}"
 
         if [[ "$state" == "live" ]]; then
 
             # download the file if it isn't archived
-            dx download "$coverage_file" -f
+            dx download "$file_id" -f
 
-            # get mean coverage values for exons 3 and 27
-            exon3=$(awk -F"\t" '$4 == "KMT2A" && $6 == "3" {print $8}' "$file_name")
-            exon27=$(awk -F"\t" '$4 == "KMT2A" && $6 == "27" {print $8}' "$file_name")
+            # check all exons of interest have >90% at 250x coverage
+            cov_issues=""
+            for exon in "3" "5" "27"; do
+                cov_250=$(awk -F"\t" -v exon="$exon" '$4=="KMT2A" && $6==exon {print $11}' "$file_name")
+                if (( $cov_250 <= 90 )); then
+                    cov_issues="${cov_issues}${exon} "
+                fi
+            done
 
-            # divide the two together
-            coverage_ratio=$(bc <<< "scale=5 ; $exon3 / $exon27")
+            # get mean coverage for exons of interest
+            exon_3=$(awk -F"\t" '$4 == "KMT2A" && $6 == "3" {print $8}' "$file_name")
+            exon_5=$(awk -F"\t" '$4 == "KMT2A" && $6 == "5" {print $8}' "$file_name")
+            exon_27=$(awk -F"\t" '$4 == "KMT2A" && $6 == "27" {print $8}' "$file_name")
 
-            # compare ratio to threshold
-            if [[ $(bc -l <<< "$coverage_ratio >= 1.1793") -eq 1 ]]; then
-                over_threshold=true
-            else
-                over_threshold=false
+            # calculate ratios
+            ratio_3_only=$(bc <<< "scale=5 ; $exon_3 / $exon_27")
+            ratio_3_and_5=$(bc <<< "scale=5 ; ($exon_3 + $exon_5) / $exon_27")
+
+            # compare ratios to thresholds
+            pass_3_only=false
+            pass_3_and_5=false
+            pass_overall=false
+
+            if [[ $(bc -l <<< "$ratio_3_only >= 1.1793") -eq 1  ]]; then
+                pass_3_only=true
+            fi
+
+            if [[ $(bc -l <<< "$ratio_3_and_5 >= 2.28548") -eq 1 ]]; then
+                pass_3_and_5=true
+            fi
+
+            if [[ "$pass_3_only" == true ]] && [[ "$pass_3_and_5" == true ]]; then
+                pass_overall=true
             fi
 
             # add to output file
-            printf "%s\t%f\t%f\t%f\t%s\n" \
-            "$sample_id" "$exon3" "$exon27" "$coverage_ratio" "$over_threshold" \
+            printf "%s\t%f\t%f\t%f\t%f\t%f\t%s\t%s\t%s\t%s\n" \
+            "$sample_id" "$exon_3" "$exon_5" "$exon_27" "$ratio_3_only" "$ratio_3_and_5" "$pass_3_only" "$pass_3_and_5" "$pass_overall" "$cov_issues" \
             >> "$output_file"
 
         else
